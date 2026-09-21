2e79734446781e2872fb93f60b188b5bbb224794
Édes Balázs <edesbalazs@gmail.com>
2026-09-21 23:18:44 +0200

    reorganized imports

--- src/web/components/editor-menu/WebEditorMenu.tsx
+++ src/web/components/editor-menu/WebEditorMenu.tsx
@@ -1,14 +1,14 @@
 import { type FC } from 'react'
 
+import { AboutGroup } from '../../../common/components/editor-menu/groups/AboutGroup'
 import { ColorsMenuGroup } from '../../../common/components/editor-menu/groups/ColorsMenuGroup'
 import { ExportMenuGroup } from '../../../common/components/editor-menu/groups/ExportMenuGroup'
-import { AboutGroup } from '../../../common/components/editor-menu/groups/AboutGroup'
 import { ScalingMenuGroup } from '../../../common/components/editor-menu/groups/ScalingMenuGroup'
 import { StepIncrementMenuGroup } from '../../../common/components/editor-menu/groups/StepIncrementMenuGroup'
 import { StitchingSettingsMenuGroup } from '../../../common/components/editor-menu/groups/StitchingSettingsMenuGroup'
 import { StitchingVisibilityMenuGroup } from '../../../common/components/editor-menu/groups/StitchingVisibilityMenuGroup'
 import { UndoRedoMenuGroup } from '../../../common/components/editor-menu/groups/UndoRedoMenuGroup'
-import { EditMenu, FileMenu, AboutMenu, ProjectMenu, ViewMenu } from '../../../common/components/editor-menu/Menus'
+import { AboutMenu, EditMenu, FileMenu, ProjectMenu, ViewMenu } from '../../../common/components/editor-menu/Menus'
 import { DownloadAppGroup } from './groups/DownloadAppGroup'
 import { DownloadMenuGroup } from './groups/DownloadMenuGroup'
 
